0c2ccec712f34091f01272c400018a96da7a129f
Maksym <m.bovsunivskyi@expoplatform.com>
2022-10-04 22:14:02 +0300

    change config user

--- testLoginLoad.js
+++ testLoginLoad.js
@@ -1,17 +1,38 @@
 import http from 'k6/http';
+import {sleep} from 'k6';
+
+export const options={
+    stages:[
+        { duration: "2s", target: 2 }
+    ],
+}
+const url = 'https://api-alpha2.expoplatform.com/api/v2/product/set';
+
+
+const params = {
+    headers: {
+        'Content-Type':'multipart/form-data',
+        'Authorization': '123456ijFEKau5TyCWaIMrfSszvFqQ7zVEMDCV'
+    },
+};
 
 export default function () {
-    const url = 'https://rctest-clone-1625730023-rc.expoplatform.com/api/v1/login';
-    const payload = JSON.stringify({
-        username: "Forest",
-        password: "Qwerty1@"
-    });
+    const formData = [{
+        exhibitor_id: 2420,
+        id: 1598,
+        name: `Product 18`,
+        event_id: 147,
+        description: '4test1'},
+        {exhibitor_id: 2420,
+            id: 1600,
+            name: 'Product 20',
+            event_id: 147,
+            description: '4test2'} ]
 
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
+    for (const param of formData){
+        http.post(url, param, params);
+        sleep(500);
 
-    http.post(url, payload, params);
+        }
+
 }
